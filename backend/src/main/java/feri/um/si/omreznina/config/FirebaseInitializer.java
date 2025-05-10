e6513ec5dfd0a196e95ae40014f6d2d7506f2049
adam8kac <adam.kac@student.um.si>
2025-05-10 19:42:20 +0200

    change: FirestoreTestSeevice added constructor

--- backend/src/main/java/feri/um/si/omreznina/config/FirebaseInitializer.java
+++ backend/src/main/java/feri/um/si/omreznina/config/FirebaseInitializer.java
@@ -8,12 +8,14 @@ import jakarta.annotation.PostConstruct;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.io.InputStream;
 import java.io.IOException;
 import java.util.List;
 
 @Configuration
+@Profile("!test")
 public class FirebaseInitializer {
 
     @Bean
