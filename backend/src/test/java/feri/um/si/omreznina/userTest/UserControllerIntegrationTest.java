30fb95ca7df26bbb68392b68b65ca610151d4865
adam8kac <adam.kac@student.um.si>
2025-05-12 23:44:54 +0200

    fix za sonarCloud - test

--- backend/src/test/java/feri/um/si/omreznina/userTest/UserControllerIntegrationTest.java
+++ backend/src/test/java/feri/um/si/omreznina/userTest/UserControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,98 +19,104 @@ import java.util.Optional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import feri.um.si.omreznina.OmrezninaApplication;
+import feri.um.si.omreznina.config.FirebaseTestConfig;
 import feri.um.si.omreznina.model.User;
 import feri.um.si.omreznina.repository.UserRepository;
 
-@SpringBootTest
+@SpringBootTest(classes = { OmrezninaApplication.class, FirebaseTestConfig.class })
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 public class UserControllerIntegrationTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+        private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @MockitoBean
-    private BCryptPasswordEncoder encoder;
+        @MockitoBean
+        private BCryptPasswordEncoder encoder;
 
-    @MockitoBean
-    private UserRepository userRepository;
+        @MockitoBean
+        private UserRepository userRepository;
 
-    @Test
-    void testUserUpdate_success() throws Exception {
-        User user = new User();
-        user.setId(1);
-        user.setEmail("test@email.com");
-        user.setPassword("password");
-        user.setFirstName("Test");
-        user.setLastName("User");
+        @Test
+        void testUserUpdate_success() throws Exception {
+                User user = new User();
+                user.setId(1);
+                user.setEmail("test@email.com");
+                user.setPassword("password");
+                user.setFirstName("Test");
+                user.setLastName("User");
 
-        mockMvc.perform(MockMvcRequestBuilders.put("/user/update")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(user)))
-                .andExpect(status().isCreated())
-                .andExpect(content().string("User updated"));
-    }
+                Mockito.when(userRepository.findById(1))
+                                .thenReturn(Optional.of(user));
 
-    @Test
-    void testUserUpdate_fail() throws Exception {
-        User user = new User();
-        user.setEmail("test@email.com");
-        user.setPassword("pass");
-        user.setFirstName("Name");
-        user.setLastName("Surname");
+                mockMvc.perform(MockMvcRequestBuilders.put("/user/update")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(user)))
+                                .andExpect(status().isCreated())
+                                .andExpect(content().string("User updated"));
+        }
 
-        mockMvc.perform(MockMvcRequestBuilders.put("/user/update")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(user)))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().string("Could not update null!"));
-    }
+        @Test
+        void testUserUpdate_fail() throws Exception {
+                User user = new User();
+                user.setEmail("test@email.com");
+                user.setPassword("pass");
+                user.setFirstName("Name");
+                user.setLastName("Surname");
 
-    @Test
-    void testUserRegistration_Successful() throws Exception {
-        User user = new User();
-        user.setEmail("test@email.com");
-        user.setPassword("pass123");
-        user.setFirstName("Janez");
-        user.setLastName("Novak");
+                mockMvc.perform(MockMvcRequestBuilders.put("/user/update")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(user)))
+                                .andExpect(status().isBadRequest())
+                                .andExpect(content().string("Could not update null!"));
+        }
 
-        Mockito.when(userRepository.findByEmail("test@email.com"))
-                .thenReturn(Optional.empty());
+        @Test
+        void testUserRegistration_Successful() throws Exception {
+                User user = new User();
+                user.setEmail("test@email.com");
+                user.setPassword("pass123");
+                user.setFirstName("Janez");
+                user.setLastName("Novak");
 
-        Mockito.when(userRepository.save(Mockito.any(User.class)))
-                .thenReturn(user);
+                Mockito.when(userRepository.findByEmail("test@email.com"))
+                                .thenReturn(Optional.empty());
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/user/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(user)))
-                .andExpect(status().isCreated())
-                .andExpect(content().string("User created"));
-    }
+                Mockito.when(userRepository.save(Mockito.any(User.class)))
+                                .thenReturn(user);
 
-    @Test
-    void testUserRegistration_Fail() throws Exception {
-        User existingUser = new User();
-        existingUser.setEmail("test@email.com");
+                mockMvc.perform(MockMvcRequestBuilders.post("/user/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(user)))
+                                .andExpect(status().isCreated())
+                                .andExpect(content().string("User created"));
+        }
 
-        User user = new User();
-        user.setEmail("test@email.com");
-        user.setPassword("pass123");
-        user.setFirstName("Janez");
-        user.setLastName("Novak");
+        @Test
+        void testUserRegistration_Fail() throws Exception {
+                User existingUser = new User();
+                existingUser.setEmail("test@email.com");
 
-        Mockito.when(userRepository.findByEmail("test@email.com"))
-                .thenReturn(Optional.of(existingUser));
+                User user = new User();
+                user.setEmail("test@email.com");
+                user.setPassword("pass123");
+                user.setFirstName("Janez");
+                user.setLastName("Novak");
 
-        Mockito.when(userRepository.save(Mockito.any(User.class)))
-                .thenReturn(user);
+                Mockito.when(userRepository.findByEmail("test@email.com"))
+                                .thenReturn(Optional.of(existingUser));
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/user/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(user)))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().string("Invalid user"));
-    }
+                Mockito.when(userRepository.save(Mockito.any(User.class)))
+                                .thenReturn(user);
+
+                mockMvc.perform(MockMvcRequestBuilders.post("/user/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(user)))
+                                .andExpect(status().isBadRequest())
+                                .andExpect(content().string("Invalid user"));
+        }
 
 }
