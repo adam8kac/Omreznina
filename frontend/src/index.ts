401b1fe641527a11da0db444c21be5b62fd0562b
Gaja <gaja.gujt1@student.um.si>
2025-05-27 21:41:01 +0200

    index.ts

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const api = axios.create({ baseURL: 'https://omreznina-app-latest.onrender.com/' });
-const api = axios.create({ baseURL: 'http://localhost:8080/' });
+const api = axios.create({ baseURL: 'https://omreznina-app-latest.onrender.com/' });
+//const api = axios.create({ baseURL: 'http://localhost:8080/' });
 
 
 export interface DayRecord {
