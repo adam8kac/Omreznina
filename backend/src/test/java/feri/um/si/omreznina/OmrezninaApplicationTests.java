3517c44c42d6704c402b2ea07a32956ea33e54c0
adam8kac <adam.kac@student.um.si>
2025-06-04 23:00:58 +0200

    fix

--- backend/src/test/java/feri/um/si/omreznina/OmrezninaApplicationTests.java
+++ backend/src/test/java/feri/um/si/omreznina/OmrezninaApplicationTests.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SuppressWarnings("removal")
 @SpringBootTest(classes = { feri.um.si.omreznina.OmrezninaApplication.class,
 		feri.um.si.omreznina.config.FirebaseTestConfig.class }, properties = {
 				"mfa.secret.encryption-key=testniKey123456", "spring.ai.openai.api-key=dummy_test_key" })
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class OmrezninaApplicationTests {
 
 	@MockBean
