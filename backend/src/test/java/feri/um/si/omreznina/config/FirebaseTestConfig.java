39d26629ca103b4f7f176b0aeb704fc417327f9e
adam8kac <adam.kac@student.um.si>
2025-06-04 21:59:03 +0200

    sonar fix

--- backend/src/test/java/feri/um/si/omreznina/config/FirebaseTestConfig.java
+++ backend/src/test/java/feri/um/si/omreznina/config/FirebaseTestConfig.java
@@ -4,6 +4,7 @@ import com.google.cloud.firestore.Firestore;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @TestConfiguration
 public class FirebaseTestConfig {
@@ -11,5 +12,10 @@ public class FirebaseTestConfig {
     @Bean
     public Firestore firestore() {
         return Mockito.mock(Firestore.class);
+    }
+
+    @Bean(name = "restTemplateForOpenAI")
+    public RestTemplate restTemplateForOpenAI() {
+        return Mockito.mock(RestTemplate.class);
     }
 }
