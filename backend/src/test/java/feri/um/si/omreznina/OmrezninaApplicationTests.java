538089fe011723ad5da4fab19dfb9ef345406032
adam8kac <adam.kac@student.um.si>
2025-06-04 21:48:24 +0200

    sonar fix

--- backend/src/test/java/feri/um/si/omreznina/OmrezninaApplicationTests.java
+++ backend/src/test/java/feri/um/si/omreznina/OmrezninaApplicationTests.java
@@ -5,14 +5,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-
+import org.springframework.web.client.RestTemplate;
 
 import feri.um.si.omreznina.config.FirebaseTestConfig;
 import feri.um.si.omreznina.service.FileService;
 import feri.um.si.omreznina.service.FirestoreService;
 
 @SuppressWarnings("removal")
-@SpringBootTest(properties = {"mfa.secret.encryption-key=testniKey123456", "spring.ai.openai.api-key=dummy_test_key"})
+@SpringBootTest(properties = { "mfa.secret.encryption-key=testniKey123456", "spring.ai.openai.api-key=dummy_test_key" })
 @ActiveProfiles("test")
 @Import(FirebaseTestConfig.class)
 class OmrezninaApplicationTests {
@@ -22,6 +22,9 @@ class OmrezninaApplicationTests {
 
 	@MockBean
 	private FirestoreService firestoreService;
+
+	@MockBean(name = "restTemplateForOpenAI")
+	private RestTemplate restTemplateForOpenAI;
 
 	@Test
 	void contextLoads() {
