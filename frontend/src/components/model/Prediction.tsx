d08f54e19ab35163ba75f33585befbb594cb4192
Gaja <gaja.gujt1@student.um.si>
2025-06-05 23:38:43 +0200

    dodan route in navigation za prediction

---
+++ frontend/src/components/model/Prediction.tsx
@@ -0,0 +1,8 @@
+export default function Prediction() {
+    return (
+        <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-2xl font-bold mb-4">Napoved</h1>
+        <p className="text-gray-600">To je komponenta za napoved</p>
+        </div>
+    );
+    }
